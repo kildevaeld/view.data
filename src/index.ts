9040455067a491717b572183eec102c3be0555a4
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-01-25 22:04:13 +0100

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from './model';
 export * from './decorators';
 export * from './array-collection';
 export * from './collection-view';
-
+export * from './types';
 export * from './model-view';
